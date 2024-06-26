ef18f55fb3b18cc07fec2a62047ad7de3b185bc0
Chaitanya <Chaitanya@CHAI-CHIRU>
2024-06-26 15:04:56 +0530

    first commit

---
+++ BATCH96COREJAVA/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method 
+		
+		Encapsulation ob= new Encapsulation();
+		ob.name="Joseph";
+		ob.age=30;
+		ob.serialNum=20;
+		ob.show();
+		}
+
+}
