c25748f7e580a13545b57fdf1e6bf9b155e38954
Chaitanya <Chaitanya@CHAI-CHIRU>
2024-06-28 13:23:25 +0530

    second commit

---
+++ BATCH96COREJAVA/src/com/chaitanya/assignment2/student.java
@@ -0,0 +1,11 @@
+package com.chaitanya.assignment2;
+
+public class student {
+
+	public class Student {
+		 public Student() {
+		        System.out.println("Student object is created");
+		    }
+		
+	}
+}
